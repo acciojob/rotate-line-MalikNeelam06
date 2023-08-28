063d3f50a7c6ed756345a24a44683869e4ebe2a8
MalikNeelam06 <128276286+MalikNeelam06@users.noreply.github.com>
2023-08-28 16:55:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //your JS code here. If required.
 let bold=getElementById("bold");
 let button=getElementById("btn");
-let degree=0;
-button.addEventListener("click",()=>{
-	degree+=5;
-	bold.style.transform=`rotate(${degree}deg)`;
-});
+// let degree=0;
+// button.addEventListener("click",()=>{
+	// degree+=5;
+	bold.style.transform=`rotate(45deg)`;
+// });
