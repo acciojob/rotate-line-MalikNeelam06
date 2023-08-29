48dbef3e2a080fcf387928f38db3ce4629854a58
MalikNeelam06 <128276286+MalikNeelam06@users.noreply.github.com>
2023-08-29 10:10:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
-let bold=getElementsBytag("hr");
+let bold=document.getElementsBytag("hr");
 // let button=getElementById("btn");
 // let degree=0;
 // button.addEventListener("click",()=>{
