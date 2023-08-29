7a8de4b217baa810dda1ddbda16735dd2e84b531
MalikNeelam06 <128276286+MalikNeelam06@users.noreply.github.com>
2023-08-29 14:28:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ let bold=document.getElementsBytag("hr");
 // let button=getElementById("btn");
 let degree=0;
 // button.addEventListener("click",()=>{
-	degree+=5;
+	
 // });
 
 setInterval(()=>{
+	degree+=5;
 		bold.style.transform=`rotate(${degree}deg)`
 },5)
